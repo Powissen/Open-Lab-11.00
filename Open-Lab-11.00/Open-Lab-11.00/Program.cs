ce207ee849e1ec451fbf63ef211f7fd8b2d59cfc
Lukáš Lokša <86780297+Powissen@users.noreply.github.com>
2022-01-28 10:18:24 +0100

    Create simple library system with two persons

--- Open-Lab-11.00/Open-Lab-11.00/Program.cs
+++ Open-Lab-11.00/Open-Lab-11.00/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace Open_Lab_11._00
 {
@@ -8,20 +8,20 @@ namespace Open_Lab_11._00
         {
             Library schoolLibrary = new Library();
 
-            schoolLibrary.studentsList.Add("John");
+            schoolLibrary.AddStudent("John");
             Book HarryPotter = new Book("Harry Potter", 230, "novela", "J.K. Rowling", 2011);
             Book Martan = new Book("Marťan", 320, "sci-fi", "Andy Weir", 2015);
-            schoolLibrary.NewBook("Harry Potter", "John");
-            schoolLibrary.NewBook("Marťan", "John");
+            schoolLibrary.AddBook(HarryPotter);
+            schoolLibrary.AddBook(Martan);
 
 
-            schoolLibrary.studentsList.Add("Poul");
+            schoolLibrary.AddStudent("Poul");
             Book TigriTim = new Book("Tigrí tím", 150, "dobrodružné", "Thomas Brezina", 2012);
             Book GrazlikGabo = new Book("Grázlik Gabo", 145, "detské", "Francesca Simonová", 2014);
-            schoolLibrary.NewBook("TigriTim", "Poul");
-            schoolLibrary.NewBook("Grázlik Gabo", "Poul");
+            schoolLibrary.AddBook(TigriTim);
+            schoolLibrary.AddBook(GrazlikGabo);
 
-            schoolLibrary.WriteAll();
+            Console.WriteLine(schoolLibrary.ToString());
         }
     }
 }
